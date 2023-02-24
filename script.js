2f9cdb47e85d73d2930e75b5d89fc908a2908dc3
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-25 00:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function majorityElement(nums) {
+  let temp = 0;
+  let abc = nums[0];
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (temp === 0) {
+      abc = nums[i];
+    }
+    
+    if (nums[i] === abc) {
+      temp++;
+    } else {
+      temp--;
+    }
+  }
+  
+  return abc;
+}
