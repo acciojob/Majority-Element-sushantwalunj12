a5625310f8720655cfef5aeb2274147750882cc3
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-17 13:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function majorityElement(nums) {
-	let map = new Map();
- 
-        for(let i = 0; i < arr.length; i++) {
-            if (map.has(arr[i])) {
-                    let count = map.get(arr[i]) +1;
-                    if (count > arr.length /2) {
-                        return arr[i];
-                    } else map.set(arr[i], count);
-            }
-            else map.set(arr[i],1);
-            }
-            return 1;
-    }
-}
+	function majorityElement(nums) {
+		let map = new Map();
+	 
+	        for(let i = 0; i < nums.length; i++) {
+	            if (map.has(nums[i])) {
+	                    let count = map.get(nums[i]) +1;
+	                    if (count > nums.length /2) {
+	                        return nums[i];
+	                    } else map.set(nums[i], count);
+	            }
+	            else map.set(nums[i],1);
+	            }
+	            return 1;
+	    }
+	}
